a7be6a339198e0b102f7a5a1aa54adbe4e4570cd
Gibril Kargbo <gibrilkargbo@Gibrils-MacBook-Pro.local>
2024-06-15 00:45:44 -0400

    done with planet1

---
+++ Solarsystem/Planet1.java
@@ -0,0 +1,8 @@
+package SolarSystem.Solarsystem;
+
+public class Planet1 {
+
+    String color;
+    String size;
+    String radius;
+}
---
+++ Solarsystem/SolarSystem.java
@@ -0,0 +1,21 @@
+package SolarSystem.Solarsystem;
+
+public class SolarSystem {
+
+    public static void main(String[] args) {
+
+        solarSystemBase solar = new solarSystemBase();
+        solar.sun ="1";
+        solar.planet ="2";
+        solar.star = "2";
+        System.out.println("the solar system is ready");
+
+        //...
+        Planet1 renox = new Planet1();
+        renox.color="grey";
+        renox.size="10km";
+        renox.radius="5m";
+
+    }
+        
+}
